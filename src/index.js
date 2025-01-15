1cd932944cc0e1abe94830509d42f7730cd6d583
RahimdadFaisalSafi <rahimdadfaisal.safi@gmail.com>
2025-01-15 20:42:27 +0100

    Add put('/notes') and delete('/notes/id') routes

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const app = express()
 app.use(express.json());
 port = 3000
 
-notes = [{
+let notes = [{
     note: 'JavaScript makes sence',
     author: 'Rahimdad Faisal Safi',
     date: '15.01.2025'
@@ -37,6 +37,40 @@ app.post('/notes', (req, res) => {
     let note = req.body
     notes.push(note)
     res.send('Note has been stored')
+
+})
+
+app.put('/notes/:id', (req, res) => {
+    let id = parseInt(req.params.id)
+    console.log(id)
+    let note = req.body
+    if (isNaN(id) || id <= 0)
+        return res.status(404).send('ID Not Found')
+    id -= 1
+    if(id in notes) {
+        notes[id] = note
+        return res.send(`Note With ID ${id} has been updated`)
+    }
+    else {
+        return res.status(404).send('ID Not Found')
+    }
+
+})
+
+app.delete('/delete/:id', (req, res) => {
+    let id = parseInt(req.params.id)
+    console.log(id)
+    let note = req.body
+    if (isNaN(id) || id <= 0)
+        return res.status(404).send('ID Not Found')
+    id -= 1
+    if(id in notes) {
+        delete notes[id]
+        return res.send(`Note With ID ${id} has been deleted`)
+    }
+    else {
+        return res.status(404).send('ID Not Found')
+    }
 
 })
 app.listen(port, () => {
